0bc65e2acd49ad28ff9779d3752cbaada7c202cf
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-14 15:35:25 +0200

    feat(examples): add BLE device scanner

---
+++ examples/scan_colmi_ring.py
@@ -0,0 +1,19 @@
+import asyncio
+from bleak import BleakScanner
+
+async def scan():
+    """Scan Colmi R02"""
+    print("Scanning BLE... press Ctrl+C to interrupt")
+    devices = await BleakScanner.discover()
+    colmi_devices=[]
+    for device in devices:
+        print(f"Found: {device}")
+        name=device.name or ""
+        if "Colmi" in name or "R02" in name:
+            colmi_devices.append((device.address, name))
+            print(f"Found Colmi: {device.name} - {device.address}")
+    if not colmi_devices:
+        print("Not found any Colmi devices")
+    
+if __name__=="__main__":
+    asyncio.run(scan())

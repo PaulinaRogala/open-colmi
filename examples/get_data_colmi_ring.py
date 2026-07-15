045e753f96726c6f06d381b9be9af2b2e42f9b4b
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-15 12:08:57 +0200

    feat(examples): add real-time HR and blood pressure reading

--- examples/get_data_colmi_ring.py
+++ examples/get_data_colmi_ring.py
@@ -1,6 +1,6 @@
 import asyncio
 from smart_ring_open.libraries.colmi_r02_client.client import Client
-from smart_ring_open.libraries.colmi_r02_client import steps
+from smart_ring_open.libraries.colmi_r02_client import steps, real_time
 from datetime import datetime
 
 async def get_data():
@@ -16,7 +16,19 @@ async def get_data():
         if isinstance(steps_data, steps.NoData):
             print("No steps data found")
         else: 
-            print(f"Steps: {steps}")
+            print(f"Steps data: {steps_data}")
+        real_time_reading_hr=await client.get_realtime_reading(real_time.RealTimeReading.HEART_RATE)
+        if real_time_reading_hr:
+            i=1
+            for hr in real_time_reading_hr:
+                print(f"Real time reading HR{i}: {hr} bpm")
+                i+=1
+        real_time_reading_pressure=await client.get_realtime_reading(real_time.RealTimeReading.BLOOD_PRESSURE)
+        if real_time_reading_pressure:
+            i=1
+            for p in real_time_reading_pressure:
+                print(f"Real time reading pressure {i}: {p}")
+                i+=1
         print("Disconnected!")
         
 if __name__=="__main__":

9bf419f0a752de3b0b610ff3e5fcfb5e3328648f
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-15 12:29:35 +0200

    refactor(examples): use returned value between functions

--- examples/get_data_colmi_ring.py
+++ examples/get_data_colmi_ring.py
@@ -2,34 +2,38 @@ import asyncio
 from smart_ring_open.libraries.colmi_r02_client.client import Client
 from smart_ring_open.libraries.colmi_r02_client import steps, real_time
 from datetime import datetime
+from scan_colmi_ring import scan
 
 async def get_data():
     """Get data from Colmi R02 by MAC address"""
-    ring_address="30:36:42:33:86:02"
-    print("Loading data...")
-    async with Client(ring_address) as client:
-        print("Connected!")
-        await client.blink_twice()
-        battery=await client.get_battery()
-        print(f"Battery level: {battery.battery_level}%")
-        steps_data=await client.get_steps(datetime.now())
-        if isinstance(steps_data, steps.NoData):
-            print("No steps data found")
-        else: 
-            print(f"Steps data: {steps_data}")
-        real_time_reading_hr=await client.get_realtime_reading(real_time.RealTimeReading.HEART_RATE)
-        if real_time_reading_hr:
-            i=1
-            for hr in real_time_reading_hr:
-                print(f"Real time reading HR{i}: {hr} bpm")
-                i+=1
-        real_time_reading_pressure=await client.get_realtime_reading(real_time.RealTimeReading.BLOOD_PRESSURE)
-        if real_time_reading_pressure:
-            i=1
-            for p in real_time_reading_pressure:
-                print(f"Real time reading pressure {i}: {p}")
-                i+=1
-        print("Disconnected!")
+    ring_address=await scan()
+    if ring_address is None:
+        print("No Colmi device found")
+    else:
+        print("Loading data from Colmi...")
+        async with Client(ring_address) as client:
+            print("Connected!")
+            await client.blink_twice()
+            battery=await client.get_battery()
+            print(f"Battery level: {battery.battery_level}%")
+            steps_data=await client.get_steps(datetime.now())
+            if isinstance(steps_data, steps.NoData):
+                print("No steps data found")
+            else: 
+                print(f"Steps data: {steps_data}")
+            real_time_reading_hr=await client.get_realtime_reading(real_time.RealTimeReading.HEART_RATE)
+            if real_time_reading_hr:
+                i=1
+                for hr in real_time_reading_hr:
+                    print(f"Real time reading HR{i}: {hr} bpm")
+                    i+=1
+            real_time_reading_pressure=await client.get_realtime_reading(real_time.RealTimeReading.BLOOD_PRESSURE)
+            if real_time_reading_pressure:
+                i=1
+                for p in real_time_reading_pressure:
+                    print(f"Real time reading pressure {i}: {p}")
+                    i+=1
+            print("Disconnected!")
         
 if __name__=="__main__":
     try:
--- examples/scan_colmi_ring.py
+++ examples/scan_colmi_ring.py
@@ -7,13 +7,14 @@ async def scan():
     devices = await BleakScanner.discover()
     colmi_devices=[]
     for device in devices:
-        print(f"Found: {device}")
+        print(f"Found: {device.address}")
         name=device.name or ""
         if "Colmi" in name or "R02" in name:
             colmi_devices.append((device.address, name))
             print(f"Found Colmi: {device.name} - {device.address}")
+            return device.address
     if not colmi_devices:
-        print("Not found any Colmi devices")
-    
+        return None
+   
 if __name__=="__main__":
     asyncio.run(scan())

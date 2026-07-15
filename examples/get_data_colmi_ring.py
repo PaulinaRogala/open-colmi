9c11841da6f1564585e110b3dda9c54e0e5a7bca
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-15 10:35:59 +0200

    feat(examples): add steps data reading

--- examples/get_data_colmi_ring.py
+++ examples/get_data_colmi_ring.py
@@ -1,22 +1,27 @@
 import asyncio
 from smart_ring_open.libraries.colmi_r02_client.client import Client
+from smart_ring_open.libraries.colmi_r02_client import steps
+from datetime import datetime
 
 async def get_data():
     """Get data from Colmi R02 by MAC address"""
     ring_address="30:36:42:33:86:02"
-    print("Loading battery level...")
+    print("Loading data...")
     async with Client(ring_address) as client:
         print("Connected!")
+        await client.blink_twice()
         battery=await client.get_battery()
         print(f"Battery level: {battery.battery_level}%")
+        steps_data=await client.get_steps(datetime.now())
+        if isinstance(steps_data, steps.NoData):
+            print("No steps data found")
+        else: 
+            print(f"Steps: {steps}")
         print("Disconnected!")
+        
 if __name__=="__main__":
     try:
         asyncio.run(get_data())
     except Exception as e:
         print(f"Error: {e}")
 
-#problem; blad o braku modulu smart_ring_open 
-# i prawdopodobnie dlatego ze nie ma w pyproject build-system,
-#ale usunelam bo byl problem z `uv sync`(nie dalo sie zrobic uv sync)
-# bo byl (błąd: `AttributeError: Using uv.build_editable is not allowed`)

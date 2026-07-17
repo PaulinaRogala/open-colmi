580a4bfd2895a7e21eebcd2abbe6ec02e776f11c
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-17 08:53:32 +0200

    chore: deleted saved data

--- examples/save_data_colmi_ring.py
+++ examples/save_data_colmi_ring.py
@@ -1,7 +1,11 @@
 import asyncio
 from smart_ring_open.libraries.colmi_r02_edgeimpulse.ring import main as ring_main
 
+
 async def run_saving_data_function():
-    await ring_main(duration=10, label="test", columns=["ppg"], resample_ms=20, plot=True, ei_upload=False)
+    
+    print("Starting...")
+    await ring_main(duration=10, label="test", columns=["ppg"], resample_ms=50, plot=True, ei_upload=False)
+    print("Saved!")
 if __name__=="__main__":
     asyncio.run(run_saving_data_function())

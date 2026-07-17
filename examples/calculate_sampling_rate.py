552399740aa0944a8f03a086b76ecec7d5612f25
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-17 10:48:22 +0200

    fix(examples): use to_numeric and covert timestamps to seconds

--- examples/calculate_sampling_rate.py
+++ examples/calculate_sampling_rate.py
@@ -12,16 +12,25 @@ def calculate_sampling_rate(csv_file_path: str, window_size: int)->float:
     
     """
     df=pd.read_csv(csv_file_path, parse_dates=['timestamp'])
-    timestamps=df['timestamp']//10**9 #convert from nanoseconds to seconds
+
+    timestamps=pd.to_numeric(df['timestamp'])//10**6 #convert from miliseconds to seconds
     timestamps=timestamps[:window_size]
+    
     intervals=np.diff(timestamps)
     avg_interval=np.mean(intervals)
-    sampling_rate=1/avg_interval
-    print(f"sampling_rate:{sampling_rate}")
+    print(f"average interval: {avg_interval}")
+
+    if avg_interval>0:
+        sampling_rate=1/avg_interval 
+    else:
+        sampling_rate=0
+
+    print(f"sampling_rate: {sampling_rate}")
     return sampling_rate
+
 if __name__=="__main__":
-    file_path="file path to your csv file"
-    calculate_sampling_rate(file_path, 1000)
+    file_path="your CSV file path"
+    calculate_sampling_rate(file_path, 100)
 
    
 

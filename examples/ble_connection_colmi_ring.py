fe58c5b6bead500ede7a636d61eed7f97279a753
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-20 13:25:23 +0200

    feat(examples): add blinking twice with connecting and disconnecting

--- examples/ble_connection_colmi_ring.py
+++ examples/ble_connection_colmi_ring.py
@@ -1,20 +1,38 @@
-from bleak import BleakClient
 import asyncio
 from scan_colmi_ring import scan
+from smart_ring_open.libraries.colmi_r02_client.client import Client
 
-async def connect():
+async def connect_to_ring():
     device_address=await scan()
     if not device_address:
         return 
-    client=BleakClient(device_address, timeout=10)
-    await client.connect()
-    print(f"Connected to {device_address}!")
+    ring_client=Client(device_address)
+    
     try:
-         while True:
-              await asyncio.sleep(1)
-    except KeyboardInterrupt:
-         await client.disconnect()
+        print(f"Connecting to {device_address}...")
+        await ring_client.connect()
+        print(f"Connected to {device_address}!\n Press Ctrl+C to disconnect.")
+        
+        await ring_client.blink_twice()
+        while True:
+            try:
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                raise KeyboardInterrupt
+           
+    
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        try: 
+            await ring_client.blink_twice()
+            await ring_client.disconnect()
+            print("Disconnected!") 
+        except Exception as e:
+            print(f"Error during disconnection: {e}")
+        return
     
 
 if __name__=="__main__":
-    asyncio.run(connect())
+    try:
+        asyncio.run(connect_to_ring())
+    except KeyboardInterrupt:
+        pass

90f575df2f7a1e759ad9b0f151d13802a097656d
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-20 10:08:22 +0200

    fix(ble): correct device selection logic and add error handling

--- examples/scan_colmi_ring.py
+++ examples/scan_colmi_ring.py
@@ -1,17 +1,37 @@
 import asyncio
 from bleak import BleakScanner
-from smart_ring_open.libraries.colmi_r02_edgeimpulse.ring import select_device
 
 async def scan():
-    """Scan Colmi R02-to find MAC address"""
-    print("Scanning BLE... press Ctrl+C to interrupt")
-    device=await select_device()
-    if select_device:
+    """Scan for Colmi R02 and return its MAC address."""
+    print("Scanning for BLE devices... ")
+    devices = await BleakScanner.discover(timeout=10)
+    if not devices:
+        print("No devices found!")
+        return None
+    device= await select_device(devices)
+    if device:
         print(f"Connected to {device}!")
         return device.address
-    else:
-        print("Choose a device to connect!")
+    return None
+
+async def select_device(devices):
+    """
+    Scan for available Bluetooth devices and allow the user to select one.
+    Args: devices- available devices to connect by BLE 
+    Return: selected device or None.
+    """
+    for i, device in enumerate(devices):
+        print(f"{i+1}: {device.name} [{device.address}]")
+    try:
+        choice = int(input("Select a device by entering its number: "))
+        if choice>len(devices) or choice<1:
+            print("Invalid choice. Select an available device.")
+            return None
+        return devices[choice-1]
+    except ValueError:
+        print("Error: enter a valid number!")
         return None
+        
    
 if __name__=="__main__":
     asyncio.run(scan())

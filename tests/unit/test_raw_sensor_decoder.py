4cff1a87eee9615c480ca36329dc9f9722f57ad2
Paulina Rogala <rogala@student.agh.edu.pl>
2026-07-23 12:34:53 +0200

    test: add spo2 packet decoding test

--- tests/unit/test_raw_sensor_decoder.py
+++ tests/unit/test_raw_sensor_decoder.py
@@ -13,10 +13,23 @@ def test_decode_ppg_packet():
     assert decoded.ppg_min == 0x1F51
     assert decoded.ppg_diff == 0x00D7
 
+#test for SpO2
+def test_decode_spo2_packet():
+    raw_hex="a101007d007d0042003b01000000001a"
+    payload=bytes.fromhex(raw_hex)
+
+    decoded=decode_raw_sensor_packet(payload)
+    assert isinstance(decoded, SpO2RawData)
+    assert decoded.spo2== 0x007d
+    assert decoded.spo2_max==0x007d
+    assert decoded.spo2_min==0x0042
+    assert decoded.spo2_diff==0x003b
+
+
 #test for invalid packet
 def test_decode_invalid_packet():
     raw_hex="a105066320281f5100d700000000009b"
     payload=bytes.fromhex(raw_hex)
     
     decoded=decode_raw_sensor_packet(payload)
-    assert decoded is None
+    assert decoded is None 
